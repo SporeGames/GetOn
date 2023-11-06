0636dcfa1e582ec87b4f899f8b377a650627779a
IceisnIceUwU <liam.1@web.de>
2023-11-06 18:00:55 +0100

    Buttons fixed, Introduction color change

--- scenes/EndScreen/EndScreen.cs
+++ scenes/EndScreen/EndScreen.cs
@@ -4,15 +4,15 @@ using GetOn.scenes;
 
 public class EndScreen : Control {
    
-    private Button _downloadButton;
-    
-    public override void _Ready() {
-        _downloadButton = GetNode<Button>("DownloadButton");
-        _downloadButton.Connect("pressed", this, nameof(OnDownloadButtonPressed));
-    }
+	private Button _downloadButton;
+	
+	public override void _Ready() {
+		_downloadButton = GetNode<Button>("DownloadButton");
+		_downloadButton.Connect("pressed", this, nameof(OnDownloadButtonPressed));
+	}
 
 
-    public void OnDownloadButtonPressed() {
-        GetNode<SharedNode>("/root/SharedNode").Print();
-    }
+	public void OnDownloadButtonPressed() {
+		GetNode<SharedNode>("/root/SharedNode").Print();
+	}
 }
