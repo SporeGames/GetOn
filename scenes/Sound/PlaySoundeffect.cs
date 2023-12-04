7c025f6b801d886fe3da81d367f8323128957b34
IceisnIceUwU <liam.1@web.de>
2023-12-04 22:39:39 +0100

    Soundeffects now pauseable

--- scenes/Sound/PlaySoundeffect.cs
+++ scenes/Sound/PlaySoundeffect.cs
@@ -1,10 +1,12 @@
 using Godot;
 using System;
+using iText.IO.Util;
 
 public class PlaySoundeffect : Button
 {
 	public AudioStreamPlayer _audioStreamPlayer;
 	private bool active = false;
+	private bool paused = false;
 	
 	public override void _Ready()
 	{
@@ -16,7 +18,7 @@ public class PlaySoundeffect : Button
 
 	public void PlayPressed()
 	{
-		if (active == false)
+		if (active == false && paused == false)
 		{
 			Resource resource = GD.Load("res://scenes/Sound/assets/StopButton.png");
 			_audioStreamPlayer.Play();
@@ -27,13 +29,39 @@ public class PlaySoundeffect : Button
 
 			//this.Disabled = true;
 		}
+		else if (active && paused == false)
+		{
+			_audioStreamPlayer.StreamPaused = true;
+			paused = true;
+			Resource resource2 = GD.Load("res://scenes/Sound/assets/PlayButton.png");
+			if (resource2 is Texture texture2)
+			{
+				this.Icon = texture2;
+			}
+		}
+
+		else if (paused)
+		{
+			_audioStreamPlayer.StreamPaused = false;
+
+			
+			Resource resource = GD.Load("res://scenes/Sound/assets/StopButton.png");
+			if (resource is Texture texture)
+			{
+				Icon = texture;
+			}
+			
+			
+			paused = false;
+		}
+		
 
 		active = true;
 	}
 
 	public void AudioFinished()
 	{
-		//this.Disabled = false;
+		GD.Print("done");
 		Resource resource2 = GD.Load("res://scenes/Sound/assets/PlayButton.png");
 		if (resource2 is Texture texture2)
 		{
