ed2565ae111284b6310a41c0a34c84aad4606a83
Malfrador <malfrador@gmail.com>
2023-12-14 16:57:45 +0100

    Add some more debug info for later

--- scenes/SharedNode.cs
+++ scenes/SharedNode.cs
@@ -1,33 +1,37 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Godot;
 using Godot.Collections;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Directory = Godot.Directory;
 
 namespace GetOn.scenes {
 	// ReSharper disable once ClassNeverInstantiated.Global
+	[JsonObject(MemberSerialization.OptIn)]
 	public class SharedNode : Node2D {
 		// Debug util stuff
 		private Node2D _debugMenu;
+		private RichTextLabel _debugText;
 		private ItemList _debugMenuList;
 
 		private List<string> discoveredScenes = new List<string>();
 		
 		private Node CurrentScene { get; set; }
-		public string PlayerName { get; set; } = "No name";
+		[JsonProperty] public string PlayerName { get; set; } = "No name";
 		
-		public readonly List<string> CompletedTasks = new List<string>();
+		[JsonProperty] public readonly List<string> CompletedTasks = new List<string>();
 		
-		public int programmingPoints = 0;
-		public float programmingTime = 0;
-		public int gameStudyPoints = 0;
-		public int gameDesignPoints = 0;
-		public int gameDesignTime = 0;
-		public int narrativePoints = 0;
-		public int soundPoints = 0;
-		public int managementPoints = 0;
-		public int managementColors = 0;
+		[JsonProperty] public int programmingPoints = 0;
+		[JsonProperty] public float programmingTime = 0;
+		[JsonProperty] public int gameStudyPoints = 0;
+		[JsonProperty] public int gameDesignPoints = 0;
+		[JsonProperty] public int gameDesignTime = 0;
+		[JsonProperty] public int narrativePoints = 0;
+		[JsonProperty] public int soundPoints = 0;
+		[JsonProperty]public int managementPoints = 0;
+		[JsonProperty] public int managementColors = 0;
 
 		public bool isDragging;
 
@@ -36,6 +40,7 @@ namespace GetOn.scenes {
 			CurrentScene = root.GetChild(root.GetChildCount() - 1);
 			isDragging = false;
 			_debugMenu = GetNode<Node2D>("DebugMenu");
+			_debugText = GetNode<RichTextLabel>("DebugMenu/DebugText");
 			_debugMenuList = GetNode<ItemList>("DebugMenu/Levels");
 			DiscoverSceneFiles("res://scenes", true);
 			GD.Print("Discovered " + discoveredScenes.Count + " scenes");
@@ -43,6 +48,24 @@ namespace GetOn.scenes {
 				_debugMenuList.AddItem(scene.Replace("res://scenes/", ""), null, true);
 			}
 			_debugMenuList.Connect("item_selected", this, nameof(OnDebugMenuItemSelected));
+		}
+
+		public override void _Process(float delta) {
+			if (!_debugMenu.Visible) {
+				return;
+			}
+			_debugText.Text = "Debug \n \n" +
+			                  "\nFPS: " + Engine.GetFramesPerSecond() +
+			                  "\nCurrent scene: " + CurrentScene.Name +
+			                  "\nMemory: " + Math.Round(Performance.GetMonitor(Performance.Monitor.MemoryDynamic) / (1024 * 1024), 2) + "/" + Math.Round(Performance.GetMonitor(Performance.Monitor.MemoryDynamicMax) / (1024 * 1024), 2) + 
+									" MB | " + Math.Round(Performance.GetMonitor(Performance.Monitor.MemoryStatic) / (1024 * 1024), 2) + "/" + Math.Round(Performance.GetMonitor(Performance.Monitor.MemoryStaticMax) / (1024 * 1024), 2) + " MB" +
+			                  "\nAudio: Device: " + AudioServer.Device + 
+									" | Latency: " + Math.Round(AudioServer.GetOutputLatency(), 4) + "ms" +
+									" | Mix rate: " + AudioServer.GetMixRate() + 
+									" | Last mix: "  + Math.Round(AudioServer.GetTimeSinceLastMix(), 2) + "ms" + 
+									" | Next mix: " + Math.Round(AudioServer.GetTimeToNextMix(), 2) + "ms" +
+			                  "\nScreen: DPI: " + OS.GetScreenDpi() + " | Size: " + OS.GetScreenSize() +
+			                  "\nSharedNodeContent: " + ToJson();
 		}
 
 		private void OnDebugMenuItemSelected(int index) {
@@ -79,10 +102,13 @@ namespace GetOn.scenes {
 		}
 
 		public string ToJson() {
-			var serializer = JsonSerializer.CreateDefault();
-			var json = new StringWriter();
-			serializer.Serialize(new JsonTextWriter(json), this);
-			return json.ToString();
+			var serializer = JsonSerializer.Create();
+			var writer = new StringWriter();
+			var json = new JsonTextWriter(writer);
+			serializer.Formatting = Formatting.Indented;
+			serializer.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+			serializer.Serialize(json, this);
+			return writer.ToString();
 		}
 
 		private void DiscoverSceneFiles(string path, bool recursive) {
