ebb78e5e0dbd929af0562d37ec34bee93e179b58
Malfrador <malfrador@gmail.com>
2023-12-31 20:33:42 +0100

    Add nodes required for truck, add example

---
+++ scenes/Programming/blocks/KeyFPressedCondition.cs
@@ -0,0 +1,10 @@
+﻿using GetOn.scenes.Programming.blocks.godot;
+using Godot;
+
+namespace GetOn.scenes.Programming.blocks {
+    public class KeyFPressedCondition : ConditionNode {
+        public override bool Check() {
+            return Input.IsActionPressed("f_pressed");
+        }
+    }
+}
---
+++ scenes/Programming/blocks/MoveTruckBlock.cs
@@ -0,0 +1,18 @@
+﻿using GetOn.scenes.Programming.blocks.logic;
+
+namespace GetOn.scenes.Programming.blocks {
+    public class MoveTruckBlock : AbstractBlock {
+        public MoveTruckBlock() {
+            Name = "MoveTruckBlock";
+        }
+		
+        public override BlockVariable Execute() {
+            
+            return new BlockVariable("jumpReturn", this, true);
+        }
+		
+        public override bool Validate() {
+            return true;
+        }
+    }
+}
