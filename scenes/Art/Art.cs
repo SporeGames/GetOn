ebef56cd57d01eab8a6c4c83ff1a71048b830c5e
Rheina03 <hasesarah03@gmail.com>
2024-01-05 22:29:42 +0100

    changed all the dialog (short description of the tasks + points-dialogs) as well as person assets

--- scenes/Art/Art.cs
+++ scenes/Art/Art.cs
@@ -1,27 +1,27 @@
 using Godot;
 
 namespace GetOn.scenes.Art {
-    public class Art : Node2D {
-        private TextureButton _backButton;
-        private TextureButton _uploadButton;
-        private Node2D _comingSoonPopup;
+	public class Art : Node2D {
+		private TextureButton _backButton;
+		private TextureButton _uploadButton;
+		private Node2D _comingSoonPopup;
 
-        public override void _Ready() {
-            _backButton = GetNode<TextureButton>("BackButton");
-            _backButton.Connect("pressed", this, nameof(OnBackButtonPressed));
-            _uploadButton = GetNode<TextureButton>("UploadButton");
-            _uploadButton.Connect("pressed", this, nameof(OnUploadButtonPressed));
-            _comingSoonPopup = GetNode<Node2D>("ComingSoonPopup");
-        }
-        
-        private void OnBackButtonPressed() {
-            GetNode<SharedNode>("/root/SharedNode").SwitchScene("res://scenes/Rooms/SoundArtRoom.tscn");
-        }
-        
-        private void OnUploadButtonPressed() {
-            _comingSoonPopup.Visible = true;
-        }
-    }
+		public override void _Ready() {
+			_backButton = GetNode<TextureButton>("BackButton");
+			_backButton.Connect("pressed", this, nameof(OnBackButtonPressed));
+			_uploadButton = GetNode<TextureButton>("UploadButton");
+			_uploadButton.Connect("pressed", this, nameof(OnUploadButtonPressed));
+			_comingSoonPopup = GetNode<Node2D>("ComingSoonPopup");
+		}
+		
+		private void OnBackButtonPressed() {
+			GetNode<SharedNode>("/root/SharedNode").SwitchScene("res://scenes/Rooms/SoundArtRoom.tscn");
+		}
+		
+		private void OnUploadButtonPressed() {
+			_comingSoonPopup.Visible = true;
+		}
+	}
 }
 
 
